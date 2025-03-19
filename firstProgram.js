f8d70ca0888e82f41981888f6aa48e7afc81d01b
IvanYamorik <63371705+IvanYamorik@users.noreply.github.com>
2025-03-19 18:17:55 +0300

    added 6 chapter task's solutions
    latest is timers.js

---
+++ firstProgram.js
@@ -0,0 +1,9 @@
+let hello = "Hello World from NODE JS"
+
+console.log(hello);
+
+a = 5;
+b = 8;
+a++
+c = a + b
+console.log(c);
---
+++ global.js
@@ -0,0 +1,4 @@
+const lib = require("path");
+
+console.log('The file name is ' + lib.basename(__filename));
+console.log(`The file name is ${lib.basename(__filename)}`);
